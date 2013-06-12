217804a24e3049b49adcebb337ad7b211675ac6f
Greg Jopa <grjopa@gmail.com>
2013-06-11 21:47:05 -0700

    update views and templates w/ more site content

--- app/scripts/views/contact_view.js
+++ app/scripts/views/contact_view.js
@@ -1,7 +1,6 @@
 App.ContactView = Ember.View.extend({
   templateName: 'contact',
   mailto: function() {
-    console.log(this.controller.get('fields.email'));
     return 'mailto:' + this.controller.get('fields.email');
   }.property('controller.fields.email')
 });
--- app/scripts/views/home_view.js
+++ app/scripts/views/home_view.js
@@ -1,3 +1,18 @@
 App.HomeView = Ember.View.extend({
-  templateName: 'home'
+  templateName: 'home',
+  paragraphs: function() {
+    var paragraphs = [];
+
+    if (this.controller.get('fields')) {
+      var fields = this.controller.get('fields');
+      fields.forEach(function(field, index) {
+        // only return fields with an actual value
+        if (field.paragraph) {
+          paragraphs.push(field);
+        }
+      });
+    }
+
+    return paragraphs;
+  }.property('controller.fields')
 });
